80046015307040f847b92ab028a7abf5da210474
rzvdev <razviiandrei@gmail.com>
2021-03-09 10:55:36 +0200

    Utility

---
+++ csharp/Utility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace csharp
+{
+    class Utility
+    {
+        public void WriteColorLine(string s, string color)
+        {
+            Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), color.Substring(0, 1).ToUpper() + color.Substring(1));
+            Console.WriteLine(s);
+            Console.ResetColor();
+        }
+    }
+}
